24d7ca6b21248e1a0d373e05ff706977f704bd71
10xvick <103027108+10xvick@users.noreply.github.com>
2023-04-08 21:38:10 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>game js</h1>
+      <p>a collection of javascript arcade games</p>
+      
+    </div>
+  );
+}
